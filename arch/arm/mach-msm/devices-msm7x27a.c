734d6c1234439a8dc503e32a2b8ca3d25a90618c
Sudhakara Rao Tentu <srtentu@codeaurora.org>
2012-03-30 15:25:18 -0700

    msm: board-msm7x27a: Increase Idle timeout
    For 7x27a and 7x27, clocks are controlled by modem. To improve stability,
    reduce number of interactions to modem to turn on and off GPU clocks.
    
    CRs-Fixed: 339805
    Signed-off-by: Sudhakara Rao Tentu <srtentu@codeaurora.org>
    (cherry picked from commit 38ad6e103486a370f29dc2625dd59cc7a2162baa)
    
    Conflicts:
    
    	arch/arm/mach-msm/board-qrd7627a.c
    	arch/arm/mach-msm/devices-msm7x2xa.h
    
    Change-Id: I65503fcb6b2949a3a5925cd6190486b6e17116d6
    Signed-off-by: Ram Kumar Chakravarthy Chebathini <rcheba@codeaurora.org>
    Signed-off-by: Lynus Vaz <lvaz@codeaurora.org>

--- arch/arm/mach-msm/devices-msm7x27a.c
+++ arch/arm/mach-msm/devices-msm7x27a.c
@@ -673,7 +673,7 @@ static struct kgsl_device_platform_data kgsl_3d0_pdata = {
 	.init_level = 0,
 	.num_levels = 3,
 	.set_grp_async = set_grp_xbar_async,
-	.idle_timeout = HZ/5,
+	.idle_timeout = HZ,
 	.nap_allowed = false,
 	.clk_map = KGSL_CLK_CORE | KGSL_CLK_IFACE | KGSL_CLK_MEM,
 };
