209b44e48e60b76ffbf41f7371c92747576b6e3f
Ulf Hansson <ulf.hansson@stericsson.com>
2012-04-18 13:13:48 +0530

    mmc: core: Detect card removal on I/O error
    To prevent I/O as soon as possible at card removal, a new
    detect work is re-scheduled without a delay to let a rescan
    remove the card device as soon a possible.
    
    Additionally, MMC_CAP2_DETECT_ON_ERR can now be used to handle
    "slowly" removed cards that a scheduled detect work did not
    detect as removed. To prevent further I/O requests for these
    lingering removed cards, check if card has been removed and then
    schedule a detect work to properly remove it.
    
    Signed-off-by: Ulf Hansson <ulf.hansson@stericsson.com>
    [subhashj@codeaurora.org: Fixed merge conflicts]
    Signed-off-by: Subhash Jadavani <subhashj@codeaurora.org>
    (cherry picked from commit c331348e4d5618259d504bbed4a8989b59c17e33)
    
    CRs-fixed: 354758
    Conflicts:
    
    	include/linux/mmc/host.h
    
    Change-Id: I28e8ab0c6ea391a457255aa4c922fd957d43d6b0
    Signed-off-by: Ramakrishna Prasad N <crpn@codeaurora.org>

--- include/linux/mmc/host.h
+++ include/linux/mmc/host.h
@@ -216,7 +216,7 @@ struct mmc_host {
 	unsigned int		caps2;		/* More host capabilities */
 
 #define MMC_CAP2_BOOTPART_NOACC	(1 << 0)	/* Boot partition no access */
-
+#define MMC_CAP2_DETECT_ON_ERR	(1 << 8)	/* On I/O err check card removal */
 	mmc_pm_flag_t		pm_caps;	/* supported pm features */
 
 	int			clk_requests;	/* internal reference counter */
