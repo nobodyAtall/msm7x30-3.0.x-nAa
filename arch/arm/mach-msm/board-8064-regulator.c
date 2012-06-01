432b1fa78dfe970ad57dffee2d8b88dc7d6e5fd8
Subhash Jadavani <subhashj@codeaurora.org>
2012-06-01 15:34:46 +0530

    mmc: msm_sdcc: merge VCCQ and VDDP regulator as VDD_IO regulator
    Typically eMMC card have IO voltage different than card VDD
    voltage and specification names it as VCCQ voltage. Currently
    driver refers regulator powering VCCQ as sdc_vccq.
    
    UHS-I (Ultra High Speed) SD cards may also have the IO voltage
    different then card VDD voltage and there is no specification
    defined name for it so driver refers regulator powering this
    SD card IO voltage as sdc_vddp.
    
    But ultimately both are referring to regulators powering IO voltage
    so it makes sense to refer both (sdc_vccq & sdc_vddp) as
    sdc_vdd_io regulator. So this patch combines them as sdc_vdd_io
    regulator.
    
    CRs-Fixed: 369258
    Change-Id: Ieafffbaebc9843c0b8683b87d2bada6b7666c142
    Signed-off-by: Subhash Jadavani <subhashj@codeaurora.org>
    
    Conflicts:
    
    	arch/arm/mach-msm/board-8930-regulator.c
    	drivers/mmc/host/msm_sdcc.c
    
    Signed-off-by: Subhash Jadavani <subhashj@codeaurora.org>

--- arch/arm/mach-msm/board-8064-regulator.c
+++ arch/arm/mach-msm/board-8064-regulator.c
@@ -51,7 +51,7 @@ VREG_CONSUMERS(L6) = {
 };
 VREG_CONSUMERS(L7) = {
 	REGULATOR_SUPPLY("8921_l7",		NULL),
-	REGULATOR_SUPPLY("sdc_vddp",		"msm_sdcc.3"),
+	REGULATOR_SUPPLY("sdc_vdd_io",		"msm_sdcc.3"),
 };
 VREG_CONSUMERS(L8) = {
 	REGULATOR_SUPPLY("8921_l8",		NULL),
@@ -124,7 +124,7 @@ VREG_CONSUMERS(S3) = {
 };
 VREG_CONSUMERS(S4) = {
 	REGULATOR_SUPPLY("8921_s4",		NULL),
-	REGULATOR_SUPPLY("sdc_vccq",		"msm_sdcc.1"),
+	REGULATOR_SUPPLY("sdc_vdd_io",		"msm_sdcc.1"),
 	REGULATOR_SUPPLY("VDDIO_CDC",		"tabla-slim"),
 	REGULATOR_SUPPLY("CDC_VDD_CP",		"tabla-slim"),
 	REGULATOR_SUPPLY("CDC_VDDA_TX",		"tabla-slim"),
