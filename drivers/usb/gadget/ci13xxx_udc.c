1a1a53e7febc559eb161dc5e8de30f57a78a6c97
Vijayavardhan Vennapusa <vvreddy@codeaurora.org>
2012-07-20 13:18:32 +0530

    USB: ci13xxx_udc: Convert ATDTW loop into finite one
    Currently while queueing requests to HW, waiting in infinite
    loop till ATDTW bit set for HW semaphore to be released by HW
    to avoid race conditions between HW and SW. This could lead to
    watchdog timer expiry if bus infrastructure is stuck. Hence
    convert this loop into finite loop of 100msec which is atmost
    required for USB HW to release HW semaphore as per HW team.
    
    CRs-Fixed: 368769
    Signed-off-by: Vijayavardhan Vennapusa <vvreddy@codeaurora.org>
    (cherry picked from commit 397a453ef048dde980b321c637d86662adbe077c)
    
    Conflicts:
    
    	drivers/usb/gadget/ci13xxx_udc.c
    
    Change-Id: I7c4a7e16ebbb8d5d0c196674708c72a306098279
    Signed-off-by: Srivalli Oguri <oguri@codeaurora.org>

--- drivers/usb/gadget/ci13xxx_udc.c
+++ drivers/usb/gadget/ci13xxx_udc.c
@@ -55,6 +55,7 @@
 #include <linux/dmapool.h>
 #include <linux/dma-mapping.h>
 #include <linux/init.h>
+#include <linux/ratelimit.h>
 #include <linux/interrupt.h>
 #include <linux/io.h>
 #include <linux/irq.h>
@@ -71,6 +72,9 @@
 /******************************************************************************
  * DEFINE
  *****************************************************************************/
+
+#define ATDTW_SET_DELAY		100 /* 100msec delay */
+
 /* ctrl register bank access */
 static DEFINE_SPINLOCK(udc_lock);
 
@@ -1665,6 +1669,7 @@ static int _hardware_enqueue(struct ci13xxx_ep *mEp, struct ci13xxx_req *mReq)
 		struct ci13xxx_req *mReqPrev;
 		int n = hw_ep_bit(mEp->num, mEp->dir);
 		int tmp_stat;
+		ktime_t start, diff;
 
 		mReqPrev = list_entry(mEp->qh.queue.prev,
 				struct ci13xxx_req, queue);
@@ -1675,9 +1680,20 @@ static int _hardware_enqueue(struct ci13xxx_ep *mEp, struct ci13xxx_req *mReq)
 		wmb();
 		if (hw_cread(CAP_ENDPTPRIME, BIT(n)))
 			goto done;
+		start = ktime_get();
 		do {
 			hw_cwrite(CAP_USBCMD, USBCMD_ATDTW, USBCMD_ATDTW);
 			tmp_stat = hw_cread(CAP_ENDPTSTAT, BIT(n));
+			diff = ktime_sub(ktime_get(), start);
+			/* poll for max. 100ms */
+			if (ktime_to_ms(diff) > ATDTW_SET_DELAY) {
+				if (hw_cread(CAP_USBCMD, USBCMD_ATDTW))
+					break;
+				printk_ratelimited(KERN_ERR
+				"%s:queue failed ep#%d %s\n",
+				 __func__, mEp->num, mEp->dir ? "IN" : "OUT");
+				return -EAGAIN;
+			}
 		} while (!hw_cread(CAP_USBCMD, USBCMD_ATDTW));
 		hw_cwrite(CAP_USBCMD, USBCMD_ATDTW, 0);
 		if (tmp_stat)
