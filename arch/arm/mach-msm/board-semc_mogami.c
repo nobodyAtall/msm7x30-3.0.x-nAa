750a9cfea457776a0125cad82831714c5b88102c
nobodyAtall <tsogkas@ceid.upatras.gr>
2013-04-07 16:20:22 +0300

    mogami-vibrator: import and fixups for 3.0

--- arch/arm/mach-msm/board-semc_mogami.c
+++ arch/arm/mach-msm/board-semc_mogami.c
@@ -219,6 +219,7 @@ static unsigned int phys_add = DDR2_BANK_BASE;
 unsigned long ebi1_phys_offset = DDR2_BANK_BASE;
 EXPORT_SYMBOL(ebi1_phys_offset);
 
+extern void msm_init_pmic_vibrator(void);
 
 static int vreg_helper_on(const char *pzName, unsigned mv)
 {
@@ -7060,6 +7061,8 @@ printk(KERN_NOTICE "msm7x30_init 13\n");
 #endif
 	hw_id_class_init();
 	shared_vreg_on();
+
+	msm_init_pmic_vibrator();
 
 	i2c_register_board_info(0, msm_i2c_board_info,
 			ARRAY_SIZE(msm_i2c_board_info));
