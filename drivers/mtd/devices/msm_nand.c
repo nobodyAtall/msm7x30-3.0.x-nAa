53044e192ebf3eb174fdd7f13b67c4aadae3c444
Marcel Bokhorst <marcel@bokhorst.biz>
2013-04-25 16:46:06 +0200

    Fix mtd density for iyokan and probably other devices

--- drivers/mtd/devices/msm_nand.c
+++ drivers/mtd/devices/msm_nand.c
@@ -746,6 +746,8 @@ uint32_t flash_onfi_probe(struct msm_nand_chip *chip)
 				supported_flash.density  =
 					onfi_param_page_ptr->
 					number_of_blocks_per_logical_unit
+					* onfi_param_page_ptr->
+					number_of_logical_units
 					* supported_flash.blksize;
 				supported_flash.ecc_correctability =
 					onfi_param_page_ptr->
