0bb5d91bd3f47138f2e29a373b4ba9c32511ed9e
Michael Bestas <mikeioannina@gmail.com>
2013-04-30 04:23:48 +0300

    leds: hardcode max_current
    Instead of relying to scripts to set the max_current at boot time, hardcode the values in the drivers like it was done for zeus board
    
    Change-Id: Ia92a0b0193803fefb3f32569e0414604855c64e9
    Signed-off-by: Michael Bestas <mikeioannina@gmail.com>

--- arch/arm/mach-msm/leds-satsuma.c
+++ arch/arm/mach-msm/leds-satsuma.c
@@ -17,31 +17,31 @@ static struct as3676_platform_led as3676_leds_mapping[] = {
 		.sinks = BIT(AS3676_SINK_01),
 		.flags = AS3676_FLAG_PWM_CTRL | AS3676_FLAG_PWM_INIT
 			| AS3676_FLAG_WAIT_RESUME,
-		.max_current = 25000,
+		.max_current = 20000,
 		.default_brightness = LED_FULL,
 	},
 	{
 		.name = "button-backlight",
 		.sinks = BIT(AS3676_SINK_RGB2),
-		.max_current = 25000,
+		.max_current = 500,
 	},
 	{
 		.name = "red",
 		.sinks = BIT(AS3676_SINK_41),
 		.flags = AS3676_FLAG_RGB | AS3676_FLAG_BLINK,
-		.max_current = 25000,
+		.max_current = 3000,
 	},
 	{
 		.name = "green",
 		.sinks = BIT(AS3676_SINK_42),
 		.flags = AS3676_FLAG_RGB | AS3676_FLAG_BLINK,
-		.max_current = 25000,
+		.max_current = 4500,
 	},
 	{
 		.name = "blue",
 		.sinks = BIT(AS3676_SINK_43),
 		.flags = AS3676_FLAG_RGB | AS3676_FLAG_BLINK,
-		.max_current = 25000,
+		.max_current = 3000,
 	},
 };
 
